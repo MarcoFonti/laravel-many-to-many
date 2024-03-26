7bfe42f57f4ede7c4eae01162b8c71e0396d917d
MarcoFonti <theemporiumg@gmail.com>
2024-03-26 16:55:11 +0100

    add migration bridge

---
+++ database/migrations/2024_03_26_153748_create_project_technology_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Project;
+use App\Models\Technology;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            $table->id();
+            /* CREO COLONNE DI CHIAVI ESTERNE */
+            /* $table->unsignedBigInteger('project_id');
+            $table->unsignedBigInteger('technology_id'); */
+
+            /* COLLEGO LE COLLONE _ID ALLE LORO TABELLE CON IL VINCOLO DELLA CHIAVE ESTERNA , ELIMINO AUTOMATICAMENTE IL RECORD SPECIFICO */
+            /* $table->foreign('project_id')->references('id')->on('projects')->constrained()->onDelete('cascade');
+            $table->foreign('technology_id')->references('id')->on('technologies')->constrained()->onDelete('cascade'); */
+            /* $table->timestamps(); */
+
+            
+            /* METODO PIU' SEMPLIFICATO */
+            $table->foreignIdFor(Project::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Technology::class)->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
